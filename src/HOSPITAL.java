75b17ecd187b0bb4ef62974b8585135cdabafa4b
Guillermo2013 <memojose_02@hotmail.com>
2013-09-11 21:50:58 -0600

    main avance

--- src/HOSPITAL.java
+++ src/HOSPITAL.java
@@ -4,16 +4,11 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.Scanner;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Guillermo Sandoval
  */
-public class HOSPITAL {
+public final class HOSPITAL {
     private RandomAccessFile DOCTORES;
     private RandomAccessFile codigoDoctores;
     private RandomAccessFile PACIENTES;
@@ -24,9 +19,9 @@ public class HOSPITAL {
     private Scanner sc = new Scanner(System.in);   
     
     public HOSPITAL() {
-    try{
-    
-        creardirs();
+ 
+   try{  
+       creardirs();
         DOCTORES=new RandomAccessFile("HOSPITAL/DOCTORES/Doctores.mnt", "rw");
         PACIENTES=new RandomAccessFile("HOSPITAL/PACIENTES/Pacientes.mnt", "rw");
         codigoDoctores=new RandomAccessFile("HOSPITAL/DOCTORES/codidosDoctores.mnt", "rw");
@@ -34,12 +29,13 @@ public class HOSPITAL {
         tipoPaciente = new RandomAccessFile("HOSPITAL/PACIENTES/tipoPacientes.mnt", "rw");
         citas=new RandomAccessFile("HOSPITAL/CITAS/citas.jhp", "rw");
         codigoCitas=new RandomAccessFile("HOSPITAL/CITAS/codigoCitas.jhp", "rw");
-    }catch(IOException e){
+        initfile();
+   }catch(IOException e){
         System.out.println("ERRRO : "+e.getMessage());
     }
     
     }
-      private void creardirs() {
+      public void creardirs() {
         File doct = new File("HOSPITAL/DOCTORES");
         doct.mkdirs();
         File pasci = new File("HOSPITAL/PACIENTES");
@@ -48,54 +44,36 @@ public class HOSPITAL {
         cita.mkdirs();
     }
       private void initfile() throws IOException{
-        if(codigoDoctores.length() == 0&&codigoPacientes.length()==0){
+        if(codigoDoctores.length() == 0&&codigoPacientes.length()==0&&codigoCitas.length()==0){
             codigoDoctores.writeInt(1);
             codigoPacientes.writeInt(1);
             codigoCitas.writeInt(1);
         }
       }
-      private int getCodigoDoctores()throws IOException{
+      public int getCodigoDoctores()throws IOException{
         codigoDoctores.seek(0);
         int codigo = codigoDoctores.readInt();
         codigoDoctores.seek(0);
         codigoDoctores.writeInt(codigo+1);
         return codigo;
     }
-          private int getCodigoPaciente()throws IOException{
+      public int getCodigoPaciente()throws IOException{
         codigoPacientes.seek(0);
         int codigo = codigoPacientes.readInt();
         codigoPacientes.seek(0);
         codigoPacientes.writeInt(codigo+1);
         return codigo;
     }
-            private int getCodigoCita()throws IOException{
+      public int getCodigoCita()throws IOException{
         codigoCitas.seek(0);
         int codigo = codigoCitas.readInt();
         codigoCitas.seek(0);
         codigoCitas.writeInt(codigo+1);
         return codigo;
     }   
-          private void añadirPaciente()throws IOException{
-              PACIENTES.seek(PACIENTES.length());
-              System.out.println("Ingrese el nombre completo:");
-              String nom=sc.next();
-              System.out.println("Direccion de telefono:");
-              int telefono=sc.nextInt();
-              System.out.println("Ingrese el numero de edad:");
-              int edad=sc.nextInt();
-              
-              
-              
-              PACIENTES.writeInt(getCodigoDoctores());
-              PACIENTES.writeUTF(nom);
-              PACIENTES.writeInt(telefono);
-              PACIENTES.writeInt(edad);
-              PACIENTES.writeUTF(especialidad);
-              PACIENTES.writeBoolean(true);
-              
+       
           
-          }
-          private void añadirDoctor()throws IOException{
+          public void añadirDoctor()throws IOException{
               DOCTORES.seek(DOCTORES.length());
               System.out.println("Ingrese el nombre completo:");
               String nom=sc.next();
@@ -105,20 +83,21 @@ public class HOSPITAL {
               int telefono=sc.nextInt();
               System.out.println("Ingrese la expecialidad");
               String especialidad=sc.next();
-              
+              boolean actividad=true;
               
               DOCTORES.writeInt(getCodigoDoctores());
               DOCTORES.writeUTF(nom);
               DOCTORES.writeUTF(domicilio);
               DOCTORES.writeInt(telefono);
               DOCTORES.writeUTF(especialidad);
-              DOCTORES.writeBoolean(true);
+              DOCTORES.writeBoolean(actividad);
              }
-          private boolean buscarDoctor(int id)throws IOException{
+          public boolean buscarDoctor(int id)throws IOException{
               DOCTORES.seek(0);
-              boolean busqueda;
+              boolean busqueda=false;
               int codigo;
-             do{
+             while(DOCTORES.getFilePointer()<DOCTORES.length()){             
+              
                   codigo=DOCTORES.readInt();
                  if (codigo==id){
                     busqueda=true;
@@ -129,17 +108,16 @@ public class HOSPITAL {
                  DOCTORES.readInt();
                  DOCTORES.readUTF();
                  DOCTORES.readBoolean();
-                    if (DOCTORES.getFilePointer()>=DOCTORES.length())
+                    if (DOCTORES.getFilePointer()==DOCTORES.length())
                          System.out.println("No se a encontrado id");
                  }
-                 
-             
-             }while(codigo!=id&&DOCTORES.getFilePointer()>=DOCTORES.length());             
+             }
+                          
               
              return busqueda;
           }
 
-            private void cambiarActivo()throws IOException{
+            public void cambiarActivo()throws IOException{
                 System.out.println("Ingrese el id que desea:");
                 int id=sc.nextInt();
                 if (buscarDoctor(id)){
@@ -165,4 +143,5 @@ public class HOSPITAL {
                 
                 }
             }
-}
+
+     }
--- src/HospitalMain.java
+++ src/HospitalMain.java
@@ -1,3 +1,7 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -9,7 +13,34 @@
  */
 public class HospitalMain {
  public static void main(String[] args) {
-        HOSPITAL h = new HOSPITAL();
-    
+        HOSPITAL hpt = new HOSPITAL();
+        
+        Scanner sc=new Scanner(System.in);
+        
+        int menu=0;
+        
+        try{
+            System.out.println("MENU\n-----\n 1.Mantenimiento\n 2.Generar Citas \n 3.Cancelar cita \n 4.Atender cita \n 5.Reporte \n 6.salir");
+            menu=sc.nextInt();
+
+            switch(menu) {
+                case 1 :
+                         System.out.println("SUBMENU DE MANTENIMIENTO\n -----\n 1.Agregar doctor \n 2.Dar de alta/baja de doctor \n 3.Agregar paciente  \n 4.Dar de baja a paciente \n 5.Agregar tipo de paciente \n 6.salir a menu principal");
+                         int respuesta=sc.nextInt();
+                         switch(respuesta){
+                             case 1:
+                                 hpt.añadirDoctor();
+                                 break;
+                         }
+
+                        break;
+
+           }
+       }catch(IOException a){
+            System.out.println("Error:"+a.getMessage());
+       }catch(Exception e){
+            System.out.println("Error:");
+       }
+ 
  }   
 }
